b384771bfa9d8c0ac16dc4c96b499fc124f1a34d
Hans Marquez <hans.marquez.ramos@gmail.com>
2023-07-24 22:35:25 -0500

    Add new files

--- 01-dart-intro/08_getters_setters.dart
+++ 01-dart-intro/08_getters_setters.dart
@@ -1,1 +1,28 @@
-void main() {}
+void main() {
+  final mySquare = new Square(side: -10);
+  // mySquare.side = -5;
+  print('Area: ${mySquare.calculateArea()}');
+}
+
+class Square {
+  double _side;
+
+  Square({required double side})
+      : assert(side >= 0,
+            'side must be >= 0'), // allow to check value with assertion before to assign to variable
+        _side = side;
+
+  double get area {
+    return _side * _side;
+  }
+
+  set side(double value) {
+    print('setting new value $value');
+    if (value < 0) throw 'Value muest be greater than zero';
+    _side = value;
+  }
+
+  double calculateArea() {
+    return _side * _side;
+  }
+}
