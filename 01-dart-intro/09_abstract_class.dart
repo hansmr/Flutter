3f9bae60fe3b8b0dd5cb2052ef770abd2ad879a9
Hans Marquez <hans.marquez.ramos@gmail.com>
2023-07-25 20:23:34 -0500

    Add file

--- 01-dart-intro/09_abstract_class.dart
+++ 01-dart-intro/09_abstract_class.dart
@@ -1,5 +1,13 @@
 void main() {
-  // final windPlant = EnergyPlant();
+  final windPlant = WindPlant(initialEnergy: 100);
+  print('wind: ${chargePhone(windPlant)}');
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Not enough energy');
+  }
+  return plant.energyLeft - 10;
 }
 
 enum PlantType { nuclear, wind, water }
@@ -11,4 +19,14 @@ abstract class EnergyPlant {
   EnergyPlant({required this.energyLeft, required this.type});
 
   void consumeEnergy(double amount);
+}
+
+// extends o implements
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initialEnergy})
+      : super(energyLeft: initialEnergy, type: PlantType.wind);
+
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
 }
