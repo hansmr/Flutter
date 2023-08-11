9c74686c660887df050a3404fe320d094862595e
Hans Marquez <hans.marquez.ramos@gmail.com>
2023-08-11 09:01:03 -0500

    Update

--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -43,7 +44,9 @@ class _ChatView extends StatelessWidget {
                 },
               ),
             ),
-            const Text('Mundo'),
+
+            /// Caja de texto de mensajes
+            const MessageFieldBox(),
           ],
         ),
       ),
---
+++ yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final FocusNode focusNode =
+        FocusNode(); // Evita que se esconda el teclado al manejar la informacion
+
+    final outlineInputBorder = UnderlineInputBorder(
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(20));
+
+    final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('Button $textValue');
+          textController.clear();
+        },
+        icon: const Icon(Icons.send_outlined),
+      ),
+    );
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('submit value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+    );
+  }
+}
